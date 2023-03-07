62b8f81f26d7dd2dd527c5c6b8b7c9a616ea0859
evanhutagaol <evanhutagaol15@gmail.com>
2023-03-07 15:33:31 +0700

    initial commit

--- src/components/Mitigasi.js
+++ src/components/Mitigasi.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { CardGroup } from 'react-bootstrap';
 
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 export const NavbarComponent = () => {
