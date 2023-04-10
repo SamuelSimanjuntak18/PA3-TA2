6ea7421d8974c61b304ad8379727f348d237f2c3
SamuelSimanjuntak <82867282+SamuelSimanjuntak18@users.noreply.github.com>
2023-04-10 20:57:54 +0700

    mitigasi

--- src/pages/Mitigasi.js
+++ src/pages/Mitigasi.js
@@ -1,12 +1,25 @@
-import React from 'react';
 // import Card from 'react-bootstrap/Card';
 // import { CardGroup } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import MitigasiBencana from '../assets/images/mitigasi bencana.png';
 import EdukasiGempa from '../assets/images/edukasi gempa.png';
 import PeringatanDini from '../assets/images/peringatan dini.png';
 import '../styles/mitigasi.css';
+import { instance } from '../apis/axios';
 
 function Mitigasi() {
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    instance
+      .get('/mitigasi/bencana')
+      .then((response) => {
+        setReports(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     
     <div className="position-relative">
@@ -27,14 +40,25 @@ function Mitigasi() {
           </div>
         </div>
       </div>
-      <div className="container mb-5 buku-panduan">
+      {reports ? (
+                reports.slice(0, 3).map((report) => (
+                   <div className="container mb-5 buku-panduan">
         <div className="row row-cols-1 row-cols-md-3 g-4">
           <div className="col-md-3">
             <div className="card mx-auto">
-              <img src={EdukasiGempa} className="card-img-top" alt="..." />
+              {/* <img src={EdukasiGempa} className="card-img-top" alt="..." /> */}
+              <img
+               className="card-img-top"
+                        style={{ borderRadius: '5px 5px 0 0' }}
+                        src={`http://localhost:8000/mitigasi/${report.attributes.gambar}`}
+                        alt={report.attributes.gambar}
+                      />
               <div className="card-body">
                 <p className="card-text">
-                  Pentingnya Sistem Peringatan Dini Bencana
+                {report.attributes.title}
+                </p>
+                <p>
+                {report.attributes.deskripsi}
                 </p>
               </div>
               <div className="card-footer">
@@ -79,6 +103,12 @@ function Mitigasi() {
           </div>
         </div>
       </div>
+                ))
+                ) : (
+                  <>
+                    <p>Loading</p>
+                  </>
+     )}
       <div className="container">
         <div className="row">
           <div className="col-lg-4 mb-4">
