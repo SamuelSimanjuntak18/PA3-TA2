fe895b34e66e48bd9c199b62dc55156fd3d4727f
SamuelSimanjuntak <82867282+SamuelSimanjuntak18@users.noreply.github.com>
2023-04-11 10:07:03 +0700

    showmitigasi

--- src/pages/Mitigasi.js
+++ src/pages/Mitigasi.js
@@ -1,5 +1,3 @@
-// import Card from 'react-bootstrap/Card';
-// import { CardGroup } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import MitigasiBencana from '../assets/images/mitigasi bencana.png';
 import EdukasiGempa from '../assets/images/edukasi gempa.png';
@@ -41,19 +39,56 @@ function Mitigasi() {
         </div>
       </div>
       {reports ? (
-                reports.slice(0, 3).map((report) => (
-                   <div className="container mb-5 buku-panduan">
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          <div className="col-md-3">
-            <div className="card mx-auto">
-              {/* <img src={EdukasiGempa} className="card-img-top" alt="..." /> */}
-              <img
-               className="card-img-top"
-                        style={{ borderRadius: '5px 5px 0 0' }}
-                        src={`http://localhost:8000/mitigasi/${report.attributes.gambar}`}
-                        alt={report.attributes.gambar}
-                      />
-              <div className="card-body">
+  <div className="container mb-5 buku-panduan">
+    <div className="row row-cols-1 row-cols-md-4 g-4">
+      {reports.filter(report => report.attributes.file.includes('.pdf')).slice(0, 3).map((report, index) => (
+        <div className="col-md-3" key={index}> 
+          <div className="card mx-auto" style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
+            <img
+              className="card-img-top"
+              style={{ borderRadius: '5px 5px 0 0', height: '150px', objectFit: 'cover' }} 
+              src={`http://localhost:8000/mitigasi/${report.attributes.file}`}
+              alt={report.attributes.file}
+            />
+            <div className="card-body" style={{ flexGrow: 1 }}> 
+              <p className="card-text">
+                {report.attributes.title}
+              </p>
+              <p>
+                {report.attributes.deskripsi}
+              </p>
+              <div className="card-footer">
+                  <p>Tinjau Edukasi</p>
+                </div>
+            </div>
+          </div>
+        </div>
+        ))
+      }
+    </div>
+  </div>
+) : (
+  <p>Loading</p>
+)}
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 mb-4">
+            <h5>VIDEO PANDUAN EDUKASI BENCANA</h5>
+          </div>
+        </div>
+        
+        <div className="container mb-5 buku-panduan">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+          {reports ? (
+                reports.filter(report => report.attributes.file.includes('.mp4')).slice(0, 3).map((report) => (
+            <div className="col-md-3">
+              <div className="card mx-auto">
+                
+              <video controls>
+        <source src={`http://localhost:8000/mitigasi/${report.attributes.file}`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="card-body">
                 <p className="card-text">
                 {report.attributes.title}
                 </p>
@@ -61,116 +96,22 @@ function Mitigasi() {
                 {report.attributes.deskripsi}
                 </p>
               </div>
-              <div className="card-footer">
-                <p>Tinjau Edukasi</p>
+                <div className="card-footer">
+                  <p>Tinjau Edukasi</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card mx-auto">
-              <img src={PeringatanDini} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <p className="card-text">
-                  Pentingnya Sistem Peringatan Dini Bencana
-                </p>
-              </div>
-              <div className="card-footer">
-                <p>Tinjau Edukasi</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card mx-auto">
-              <img src={EdukasiGempa} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <p className="card-text">Siaga Bencana Gempa Bumi</p>
-              </div>
-              <div className="card-footer">
-                <p>Tinjau Edukasi</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div className="card mx-auto">
-              <img src={PeringatanDini} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <p className="card-text">Siaga Bencana Gempa Bumi</p>
-              </div>
-              <div className="card-footer">
-                <p>Tinjau Edukasi</p>
-              </div>
-            </div>
+              ))
+              ) : (
+                <>
+                  <p>Loading</p>
+                </>
+                  )}
           </div>
         </div>
       </div>
-                ))
-                ) : (
-                  <>
-                    <p>Loading</p>
-                  </>
-     )}
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 mb-4">
-            <h5>VIDEO PANDUAN EDUKASI BENCANA</h5>
-          </div>
-        </div>
-        <div className="container mb-5 buku-panduan">
-          <div className="row row-cols-1 row-cols-md-3 g-4">
-            <div className="col-md-3">
-              <div className="card mx-auto">
-                <img src={EdukasiGempa} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">
-                    Pentingnya Sistem Peringatan Dini Bencana
-                  </p>
-                </div>
-                <div className="card-footer">
-                  <p>Tinjau Edukasi</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="card mx-auto">
-                <img src={PeringatanDini} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">
-                    Pentingnya Sistem Peringatan Dini Bencana
-                  </p>
-                </div>
-                <div className="card-footer">
-                  <p>Tinjau Edukasi</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="card mx-auto">
-                <img src={EdukasiGempa} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">Siaga Bencana Gempa Bumi</p>
-                </div>
-                <div className="card-footer">
-                  <p>Tinjau Edukasi</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="card mx-auto">
-                <img src={PeringatanDini} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <p className="card-text">Siaga Bencana Gempa Bumi</p>
-                </div>
-                <div className="card-footer">
-                  <p>Tinjau Edukasi</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
     </div>
-    
+  
   );
 }
 
